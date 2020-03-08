cd35b1b092500cbc40d4a82b546410ea388c07fb
Alexandre Hachim <alex100dre.pro@gmail.com>
2020-03-08 22:48:46 +0100

    feat(7): display a timeline with events

---
+++ src/components/Timeline/Timeline.style.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+const lineWidth = 3;
+export const dotSize = 21;
+
+
+export const TimelineStyled = styled.ul`
+    position: relative;
+    list-style-type: none;
+    width: 70%;
+    padding: 0;
+    margin: 50px auto;
+
+    &:after {
+        content: '';
+        width: ${lineWidth}px;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: ${dotSize * 0.5 - lineWidth * 0.5}px;
+        background: black;
+    }
+`;
