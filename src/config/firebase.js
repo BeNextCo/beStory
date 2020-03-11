6ddb84bb81f2bb00d397f023f5a10c95e9064cd8
Alexandre Hachim <alex100dre.pro@gmail.com>
2020-03-11 18:09:35 +0100

    feat(13): firebase hosting

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0F_iIslJoqcOp5meLx02CSmxaRgLXS8Q",
+    authDomain: "bestory-90e7e.firebaseapp.com",
+    databaseURL: "https://bestory-90e7e.firebaseio.com",
+    projectId: "bestory-90e7e",
+    storageBucket: "bestory-90e7e.appspot.com",
+    messagingSenderId: "154490843707",
+    appId: "1:154490843707:web:f2fe5dfa5ae82b94a6221e"
+};
+
+export default firebaseConfig;
